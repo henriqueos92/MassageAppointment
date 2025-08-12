e1071fc8f2911a25af7bc1ef5da523028bba1d09
Henrique <henriqueos.92@outlook.com>
2025-08-12 17:02:03 -0300

    Implementado pagina de bloqueio ao tentar acessar o devtools

---
+++ public/devtools-detect.js
@@ -0,0 +1,23 @@
+let devtoolsAtivo = false;
+const paginaOriginal = '/';
+const paginaBloqueio = '/pagina-bloqueio.html';
+
+setInterval(function () {
+    const aberto = window.outerWidth - window.innerWidth > 200 || window.outerHeight - window.innerHeight > 200;
+
+    if (aberto && !devtoolsAtivo) {
+        devtoolsAtivo = true;
+
+        if (window.location.pathname !== paginaBloqueio) {
+            window.location.href = paginaBloqueio;
+        }
+    }
+
+    if (!aberto && devtoolsAtivo) {
+        devtoolsAtivo = false;
+
+        if (window.location.pathname === paginaBloqueio) {
+            window.location.href = paginaOriginal;
+        }
+    }
+}, 500);
